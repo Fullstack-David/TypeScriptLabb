696272fb95ee67fc379ad13e3a1a384a21a1aaec
David Heidari <davidheidari2@gmail.com>
2024-04-30 14:52:45 +0200

    add css and fixed useEffect-bug

--- Labb-project/src/Board.tsx
+++ Labb-project/src/Board.tsx
@@ -9,6 +9,11 @@ export default function Board() {
   );
   const [winner, setWinner] = useState<Player>(null);
 
+  //  Mapapr över cellerna
+  //  om index i är lika med indexValue så returnar den currentPlayer
+  //  Annars retuneras det ursprungliga värdet i cellen
+  //  Uppdaterar cellerna med den nya datan
+  //  Om currentPlayer är lika med "X" så sätter den till "O" annars "X"
   function setCellValue(index: number) {
     const newData = cells.map((val, i) => {
       if (i === index) {
@@ -21,13 +26,20 @@ export default function Board() {
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
 
+  // Tömmer arrayen och sätter alla värden til noll
+  // nollsätter vinnarstatus
+  // Väljer en slumpmässigt startspelare om indexet är lika med 1 så kör den "X" annars "O"
   function reset() {
     setCells(Array(9).fill(null));
     setWinner(null);
     setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
   }
-
-  const checkWin = (cells: any) => {
+  // CheckWin funktionen bestämmer om det finns en vinnare i spelet baserat på de aktuella cellernas tillstånd.
+  // Parametern 'cells' är en array av Player där varje element representerar spelaren i en cell ('X', 'O' eller null).
+  // Funktionen itererar över definierade linjer (rader, kolumner, diagonaler) för att kontrollera om en spelare har tre i rad.
+  // Om alla tre positioner i någon linje är ockuperade av samma spelare (och inte null), returneras den spelarens värde ('X' eller 'O').
+  // Annars returneras null om ingen vinnare finns.
+  const checkWin = (cells: Player[]) => {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -46,7 +58,11 @@ export default function Board() {
     }
     return null;
   };
-
+  // useEffecten används för att bestämma spelets utfall baserat på nuvarande celltillstånd.
+  // Kallar på checkWin för att se om det finns en vinnare
+  // Uppdaterar vinnarstatus om en vinnare finns
+  // Sätter vinnarstatus till "BOTH" om spelet är oavgjort
+  // Lägger till cells i beroendelistan för att reagera på dess förändringar
   useEffect(() => {
     const w = checkWin(cells);
     if (w) {
@@ -56,11 +72,15 @@ export default function Board() {
     if (!w && !cells.filter((cell) => !cell).length) {
       setWinner("BOTH");
     }
-  }, []);
+  }, [cells]);
 
   return (
     <div>
-      {!winner && <p>Hey {currentPlayer} its your turn!</p>}
+      {!winner && (
+        <p>
+          Hey <span>{currentPlayer} </span>its your turn!
+        </p>
+      )}
       {winner && winner !== "BOTH" && <p>Congrats {winner}</p>}
       {winner && winner === "BOTH" && <p>Congrats you are both winners</p>}
       <div className="grid">
